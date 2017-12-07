0611dce55d50ed15418bcded5b7fb27f9d0f60c7
Georgios Karametas <geometas@hotmail.com>
2017-12-07 13:50:03 +0100

    build($compile): Added webpack
    Added webpack config and server

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+const paths = {
+  DIST: path.resolve(__dirname, 'dist'),
+  PUBLIC: path.resolve(__dirname, 'public'),
+  SRC: path.resolve(__dirname, 'src'),
+}
+
+module.exports = {
+  entry: path.join(paths.SRC, 'index.js'),
+  output: {
+    path: paths.PUBLIC,
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(paths.PUBLIC, 'index.html'),
+    }),
+    new ExtractTextPlugin('style.css'),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+        ],
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          use: 'css-loader',
+        }),
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+}
