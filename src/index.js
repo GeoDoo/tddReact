efb99ef464bfcdf8fb070b0586bcbdb03630e49b
Georgios Karametas <geometas@hotmail.com>
2017-12-07 14:55:22 +0100

    test: Added enzyme

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import React from 'react'
 import { render } from 'react-dom'
 import App from './components/App'
 
-render(<div>Hello</div>, document.getElementById('root') || document.createElement('div'))
+render(<App />, document.getElementById('root') || document.createElement('div'))
